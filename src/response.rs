f89b366a727a3aa04229918ea71ffa105282401c
shamxl <shamxl@pm.me>
2023-11-19 00:06:40 +0530

    add: html template response

--- src/response.rs
+++ src/response.rs
@@ -12,6 +12,19 @@ impl Response {
         header
     }
 
+    pub fn ok_with_html (content: &str) -> String {
+    	let html = include_str! ("../public/index.html");
+    	let formatted_html = html.replace("{}", content);
+		let header = format! (
+			"HTTP/1.1 200 OK\r\nContent-Length: {0}\r\nContent-Type: {1}\r\n\r\n{2}",
+			formatted_html.len(),
+			"text/html",
+			formatted_html
+		);
+
+		header
+    }
+
     pub fn not_found(content: &str, content_type: &str) -> String {
         let header = format!(
             "HTTP/1.1 404 Bad Request\r\nContent-Length: {0}\r\nContent-Type: {1}\r\n\r\n{2}",
