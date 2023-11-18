ab8383b968f99516dc994a1e28573e90cc5a48f7
shamxl <shamxl@pm.me>
2023-11-18 15:21:04 +0530

    refactor: error handling

--- src/operations/file.rs
+++ src/operations/file.rs
@@ -5,7 +5,7 @@ pub fn read_file(path: &str) -> std::io::Result<String> {
     let mut file = fs::File::open(path)?;
     let mut contents = String::new();
 
-    file.read_to_string(&mut contents);
+    file.read_to_string(&mut contents)?;
 
     Ok(contents)
 }
