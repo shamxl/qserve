fd12980217ae1b01e23d9e7a5ca9936f5d4e1e97
shamxl <shamxl@pm.me>
2023-11-22 09:56:47 +0530

    drop: home page

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,11 +1,3 @@
-use crate::log;
-use crate::response;
-use log::Logger;
-use response::Response;
-use std::{io::Write, net::TcpStream};
+use std::net::TcpStream;
 
-pub fn home(mut stream: &TcpStream, request: String) {
-
-    let html = include_str!("../../public/index.html");
-    let _ = stream.write_all(Response::ok(&html, "text/html").as_bytes());
-}
+pub fn home(mut _stream: &TcpStream, _request: String) {}
