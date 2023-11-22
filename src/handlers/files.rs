4e0c3a9d2ee7f081ea455326a1b0a239be678942
shamxl <shamxl@pm.me>
2023-11-22 09:56:03 +0530

    add: file serving

--- src/handlers/files.rs
+++ src/handlers/files.rs
@@ -1,8 +1,43 @@
+use crate::log::Logger;
+use crate::operations::file;
 use crate::response::Response;
+use std::fs;
+use std::path::Path;
 use std::{io::Write, net::TcpStream};
 
-pub fn files (mut stream: &TcpStream, request: String) {
+pub fn files(mut stream: &TcpStream, request: String) {
+    let path = request
+        .split_whitespace()
+        .nth(1)
+        .unwrap()
+        .replacen('/', "./", 1);
+    let metadata = match fs::metadata(&path) {
+        Ok(metadata) => metadata,
+        Err(e) => {
+            let _ = stream.write_all(
+                Response::bad_request("error: check terminal for logs", "text/plain").as_bytes(),
+            );
+            Logger::error(format!("{}", e));
+            return;
+        }
+    };
 
-    let content = ["<div class=\"file type-dir\"><p>src</p></div>", "<div class=\"file\"><p>Cargo.toml</p></div>"];
-    let _ = stream.write_all(Response::ok_with_html (&content.join("\n")).as_bytes());
+    if metadata.is_file() {
+        match file::read_file(&path) {
+            Ok(file_content) => {
+                let file_name = Path::new(&path).file_name().unwrap().to_str().unwrap();
+                let _ = stream.write_all(Response::send_file(file_content, file_name).as_bytes());
+            }
+            Err(e) => {
+                let _ = stream.write_all(
+                    Response::bad_request(
+                        "error while reading file, check terminal for logs",
+                        "text/plain",
+                    )
+                    .as_bytes(),
+                );
+                Logger::error(format!("{}", e));
+            }
+        };
+    }
 }
