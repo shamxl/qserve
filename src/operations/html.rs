e52987e473b5bdca3d5a434ca5c8b59902966766
shamxl <shamxl@pm.me>
2023-11-25 14:26:24 +0530

    Optimize functions

--- src/operations/html.rs
+++ src/operations/html.rs
@@ -1,6 +1,6 @@
 const TEMPLATE: &str = include_str!("../../public/index.html");
 
-fn generate(content: &str, path: &str) -> String {
+pub fn generate(content: &str, path: &str) -> String {
     TEMPLATE
         .replace("{content}", content)
         .replace("{path}", path)
