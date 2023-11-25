fd29c8ec1db0624c0e662475b04bb1aa2f04ed12
shamxl <shamxl@pm.me>
2023-11-25 11:21:02 +0530

    Drop html response

--- src/response.rs
+++ src/response.rs
@@ -12,19 +12,6 @@ impl Response {
         header
     }
 
-    pub fn ok_with_html(content: &str) -> String {
-        let html = include_str!("../public/index.html");
-        let formatted_html = html.replace("{content}", content);
-        let header = format!(
-            "HTTP/1.1 200 OK\r\nContent-Length: {0}\r\nContent-Type: {1}\r\n\r\n{2}",
-            formatted_html.len(),
-            "text/html",
-            formatted_html
-        );
-
-        header
-    }
-
     pub fn bad_request(content: &str, content_type: &str) -> String {
         let header = format!(
             "HTTP/1.1 404 Bad Request\r\nContent-Length: {0}\r\nContent-Type: {1}\r\n\r\n{2}",
