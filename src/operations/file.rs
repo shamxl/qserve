e4e6cbc3f0184fe1052f8a5e13f96c9f5552a00c
shamxl <shamxl@pm.me>
2023-11-25 14:25:58 +0530

    Add function `read_dir`

--- src/operations/file.rs
+++ src/operations/file.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::fs::DirEntry;
 use std::io::Read;
 
 pub fn read_file(path: &str) -> std::io::Result<Vec<u8>> {
@@ -8,4 +9,14 @@ pub fn read_file(path: &str) -> std::io::Result<Vec<u8>> {
     file.read_to_end(&mut contents)?;
 
     Ok(contents)
+}
+
+pub fn read_dir (path: &str) -> std::io::Result<Vec<DirEntry>> {
+	let dir = fs::read_dir(path)?;
+	let mut contents = Vec::new();
+	for e in dir {
+		contents.push(e.unwrap())
+	}
+
+	Ok(contents)
 }
