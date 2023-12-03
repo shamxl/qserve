369db86b22aea9e80258f28f283059019577bc91
shamxl <shamxl@pm.me>
2023-12-03 13:42:13 +0530

    Optimize functions

--- src/handlers/files.rs
+++ src/handlers/files.rs
@@ -1,18 +1,14 @@
-use crate::arguments::get_chunk_size;
-use crate::log::Logger;
-use crate::operations::decoder;
-use crate::operations::file;
-use crate::operations::html;
-use crate::response::Response;
-
-use std::fs;
-use std::path::Path;
-use std::{
-    io::{Read, Write},
-    net::TcpStream,
+use {
+    crate::{
+        log::Logger,
+        operations::{decoder, file},
+        response::Response,
+    },
+    file::{serve_dir, stream},
+    std::{fs, io::Write, net::TcpStream},
 };
 
-pub fn files(mut stream: &TcpStream, request: String) {
+pub fn files(mut tcpstream: &TcpStream, request: String) {
     let path = decoder::decode(
         request
             .split_whitespace()
@@ -24,7 +20,7 @@ pub fn files(mut stream: &TcpStream, request: String) {
     let metadata = match fs::metadata(&path) {
         Ok(metadata) => metadata,
         Err(e) => {
-            let _ = stream.write_all(
+            let _ = tcpstream.write_all(
                 Response::bad_request("error: check terminal for logs", "text/plain").as_bytes(),
             );
             Logger::error(format!("{}", e));
@@ -33,80 +29,8 @@ pub fn files(mut stream: &TcpStream, request: String) {
     };
 
     if metadata.is_file() {
-        match fs::File::open(&path) {
-            Ok(mut file_buffer) => {
-                let mut buffer = vec![0; get_chunk_size()];
-                let file_name = Path::new(&path).file_name().unwrap().to_str().unwrap();
-                let _ = stream.write_all(Response::send_file(metadata.len(), file_name).as_bytes());
-
-                loop {
-                    match file_buffer.read(&mut buffer) {
-                        Ok(bytes) => {
-                            if bytes == 0 {
-                                break;
-                            }
-
-                            let _ = stream.write_all(&buffer[..bytes]);
-                        }
-                        Err(e) => Logger::error(format!("file reading failed, {}", e)),
-                    }
-                }
-            }
-
-            Err(e) => {
-                let _ = stream.write_all(
-                    Response::bad_request("error: check terminal for logs", "text/plain")
-                        .as_bytes(),
-                );
-                Logger::error(format!("{}", e))
-            }
-        }
+        stream(tcpstream, metadata.len(), path);
     } else {
-        match file::read_dir(&path) {
-            Ok(dir_contents) => {
-                let mut contents = Vec::new();
-                let parent_directory = Path::new(request.split_whitespace().nth(1).unwrap())
-                    .parent()
-                    .unwrap()
-                    .display();
-                contents.push(format!(
-                    "<a class=\"back\" href=\"{}\"></a>",
-                    parent_directory
-                ));
-                for i in dir_contents {
-                    let mut file_type = String::from("file");
-                    match fs::metadata(i.path()) {
-                        Ok(f_type) => {
-                            if f_type.is_dir() {
-                                file_type = "dir".to_string()
-                            }
-                        }
-                        Err(_) => Logger::warn("unknown error"),
-                    }
-
-                    contents.push(format!(
-                        "<a class=\"{0}\" href=\"/{1}\">{2}</a>",
-                        file_type,
-                        i.path().display(),
-                        i.file_name().to_str().unwrap()
-                    ));
-                }
-
-                let _ = stream.write_all(
-                    Response::ok(&html::generate(&contents.join("\n"), &path), "text/html")
-                        .as_bytes(),
-                );
-            }
-            Err(e) => {
-                let _ = stream.write_all(
-                    Response::bad_request(
-                        "error reading dir, check terminal for logs",
-                        "text/plain",
-                    )
-                    .as_bytes(),
-                );
-                Logger::error(format!("{}", e));
-            }
-        };
+        serve_dir(tcpstream, request, path)
     }
 }
--- src/operations/file.rs
+++ src/operations/file.rs
@@ -1,5 +1,13 @@
-use std::fs;
-use std::fs::DirEntry;
+use {
+    crate::{config::Config, log::Logger, operations::html, response::Response},
+    std::{
+        fs,
+        fs::DirEntry,
+        io::{Read, Write},
+        net::TcpStream,
+        path::Path,
+    },
+};
 
 pub fn read_dir(path: &str) -> std::io::Result<Vec<DirEntry>> {
     let dir = fs::read_dir(path)?;
@@ -9,4 +17,81 @@ pub fn read_dir(path: &str) -> std::io::Result<Vec<DirEntry>> {
     }
 
     Ok(contents)
+}
+
+pub fn stream(mut stream: &TcpStream, content_len: u64, path: String) {
+    let config = Config::parse();
+
+    match fs::File::open(&path) {
+        Ok(mut file_buffer) => {
+            let mut buffer = vec![0; config.chunks];
+            let file_name = Path::new(&path).file_name().unwrap().to_str().unwrap();
+            let _ = stream.write_all(Response::send_file(content_len, file_name).as_bytes());
+
+            loop {
+                match file_buffer.read(&mut buffer) {
+                    Ok(bytes) => {
+                        if bytes == 0 {
+                            break;
+                        }
+
+                        let _ = stream.write_all(&buffer[..bytes]);
+                    }
+                    Err(e) => Logger::error(format!("file reading failed, {}", e)),
+                }
+            }
+        }
+
+        Err(e) => {
+            let _ = stream.write_all(
+                Response::bad_request("error: check terminal for logs", "text/plain").as_bytes(),
+            );
+            Logger::error(format!("{}", e))
+        }
+    }
+}
+
+pub fn serve_dir(mut stream: &TcpStream, request: String, path: String) {
+    match read_dir(&path) {
+        Ok(dir_contents) => {
+            let mut contents = Vec::new();
+            let parent_directory = Path::new(request.split_whitespace().nth(1).unwrap())
+                .parent()
+                .unwrap()
+                .display();
+            contents.push(format!(
+                "<a class=\"back\" href=\"{}\"></a>",
+                parent_directory
+            ));
+            for i in dir_contents {
+                let mut file_type = String::from("file");
+                match fs::metadata(i.path()) {
+                    Ok(f_type) => {
+                        if f_type.is_dir() {
+                            file_type = "dir".to_string()
+                        }
+                    }
+                    Err(_) => Logger::warn("unknown error"),
+                }
+
+                contents.push(format!(
+                    "<a class=\"{0}\" href=\"/{1}\">{2}</a>",
+                    file_type,
+                    i.path().display(),
+                    i.file_name().to_str().unwrap()
+                ));
+            }
+
+            let _ = stream.write_all(
+                Response::ok(&html::generate(&contents.join("\n"), &path), "text/html").as_bytes(),
+            );
+        }
+        Err(e) => {
+            let _ = stream.write_all(
+                Response::bad_request("error reading dir, check terminal for logs", "text/plain")
+                    .as_bytes(),
+            );
+            Logger::error(format!("{}", e));
+        }
+    };
 }
