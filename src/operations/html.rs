e19313eeda5dd533025d080075880455c269875a
shamxl <shamxl@pm.me>
2023-12-04 19:59:29 +0530

    Use function `get_short_path`

--- src/operations/html.rs
+++ src/operations/html.rs
@@ -1,7 +1,9 @@
+use crate::config::get_short_path;
+
 const TEMPLATE: &str = include_str!("../../public/index.html");
 
 pub fn generate(content: &str, path: &str) -> String {
     TEMPLATE
         .replace("{content}", content)
-        .replace("{path}", path)
+        .replace("{path}", &get_short_path(path.to_string()))
 }
