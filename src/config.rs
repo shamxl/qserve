16b4e5d8edb774ae4f394368c875b8872b9117fc
shamxl <shamxl@pm.me>
2023-12-08 21:06:10 +0530

    Add path operations

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,8 @@
-use std::{env, fs};
+use std::{env, fs, process::exit, path::{Path}, path::PathBuf};
 
 #[derive(Debug)]
 pub struct Config {
+	/// hi
     pub ip: String,
     pub port: String,
     pub chunks: usize,
@@ -40,8 +41,25 @@ impl Config {
         let config = Config::default();
         let args: Vec<String> = env::args().collect();
         let path = Self::parse_args(&args, "--path", "-P", config.path);
-        let config_path = fs::canonicalize(&path);
+        let config_path = fs::canonicalize(path);
+        let version = env! ("CARGO_PKG_VERSION");
+        if Self::parse_args(&args, "--version", "-v", "None".to_string()) != "None" {
+        	println! ("qserve {}", version);
+        	exit(0);
+        }
+		if Self::parse_args(&args, "--help", "-h", "None".to_string()) != "None" {
+			let message = format!("Qserve - {0}\n
+            --ip      or -i – to set the ip address	[default: 0.0.0.0]
+            --port    or -p – to set the port		[default: 3000]
+            --path    or -P – to set the path to serve	[default: . (current dir)]
+            --chunks  or -c – to set chunk size 	[default: 4096]
+            --version or -v – print version and exit
+            --help    or -h – print this message and exit 
+            ", version);
 
+            println! ("{}", message);
+  			exit(0);
+		}
         Config {
             ip: Self::parse_args(&args, "--ip", "-i", config.ip),
             port: Self::parse_args(&args, "--port", "-p", config.port),
@@ -51,3 +69,31 @@ impl Config {
     }
 }
 
+
+pub fn get_short_path(mut req_path: String) -> String {
+    let config = Config::parse();
+
+    if !req_path.starts_with('/') {
+        let formatted_path = format!("/{}", req_path);
+        req_path = formatted_path
+    }
+    let mut absolute_path = Path::new(&req_path)
+        .to_str()
+        .unwrap()
+        .replacen(&config.path, "", 1);
+
+    if absolute_path == "/." {
+        absolute_path = "./".to_string()
+    }
+
+    absolute_path
+}
+
+pub fn get_full_path(short_path: String) -> String {
+    let config = Config::parse();
+    let short_path = short_path.replacen('/', "", 1);
+    PathBuf::from(config.path)
+        .join(short_path)
+        .display()
+        .to_string()
+}
