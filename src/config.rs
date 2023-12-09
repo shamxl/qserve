93898f708ece3305c1970a5a5cc0c216193badfd
shamxl <shamxl@pm.me>
2023-12-09 09:08:03 +0530

    Add field `preview`

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,12 @@
-use std::{env, fs, process::exit, path::{Path}, path::PathBuf};
+use std::{env, fs, path::Path, path::PathBuf, process::exit};
 
 #[derive(Debug)]
 pub struct Config {
-	/// hi
     pub ip: String,
     pub port: String,
     pub chunks: usize,
     pub path: String,
+    pub preview: String,
 }
 
 impl Default for Config {
@@ -16,6 +16,7 @@ impl Default for Config {
             port: "3000".to_string(),
             chunks: 4096,
             path: ".".to_string(),
+            preview: "None".to_string(),
         }
     }
 }
@@ -40,35 +41,54 @@ impl Config {
     pub fn parse() -> Self {
         let config = Config::default();
         let args: Vec<String> = env::args().collect();
-        let path = Self::parse_args(&args, "--path", "-P", config.path);
-        let config_path = fs::canonicalize(path);
-        let version = env! ("CARGO_PKG_VERSION");
+        let path = Self::parse_args(&args, "--path", "-P", ".".to_string());
+        let mut config_path = fs::canonicalize(path);
+        let version = env!("CARGO_PKG_VERSION");
         if Self::parse_args(&args, "--version", "-v", "None".to_string()) != "None" {
-        	println! ("qserve {}", version);
-        	exit(0);
+            println!("qserve {}", version);
+            exit(0);
         }
-		if Self::parse_args(&args, "--help", "-h", "None".to_string()) != "None" {
-			let message = format!("Qserve - {0}\n
+        if Self::parse_args(&args, "--help", "-h", "None".to_string()) != "None" {
+            let message = format!(
+                "Qserve - {0}\n
             --ip      or -i – to set the ip address	[default: 0.0.0.0]
             --port    or -p – to set the port		[default: 3000]
             --path    or -P – to set the path to serve	[default: . (current dir)]
             --chunks  or -c – to set chunk size 	[default: 4096]
             --version or -v – print version and exit
             --help    or -h – print this message and exit 
-            ", version);
+            ",
+                version
+            );
 
-            println! ("{}", message);
-  			exit(0);
-		}
+            println!("{}", message);
+            exit(0);
+        }
+
+        let mut preview_file = String::from("None");
+        let mut dir_path = config_path.as_mut().unwrap().display().to_string();
+        if fs::metadata(config_path.as_mut().unwrap())
+            .unwrap()
+            .is_file()
+        {
+            preview_file = config_path.as_mut().unwrap().display().to_string();
+            dir_path = Path::new(&preview_file)
+                .parent()
+                .unwrap()
+                .display()
+                .to_string()
+        }
         Config {
             ip: Self::parse_args(&args, "--ip", "-i", config.ip),
             port: Self::parse_args(&args, "--port", "-p", config.port),
-            chunks: Self::parse_args(&args, "--chunks", "-c", config.chunks.to_string()).parse().unwrap(),
-            path: config_path.unwrap().display().to_string(),
+            chunks: Self::parse_args(&args, "--chunks", "-c", config.chunks.to_string())
+                .parse()
+                .unwrap(),
+            path: dir_path,
+            preview: preview_file,
         }
     }
 }
-
 
 pub fn get_short_path(mut req_path: String) -> String {
     let config = Config::parse();
